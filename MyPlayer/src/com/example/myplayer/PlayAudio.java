cb9d81210f467c2579d0169e666e386b1da62e68
EliseoRios <eliseohack@gmail.com>
2014-11-17 19:44:24 -0600

    videos funcionando..

---
+++ MyPlayer/src/com/example/myplayer/PlayAudio.java
@@ -0,0 +1,19 @@
+package com.example.myplayer;
+
+import android.support.v7.app.ActionBarActivity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.VideoView;
+
+public class PlayAudio extends ActionBarActivity 
+{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) 
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_play_audio);
+		
+	}
+}
